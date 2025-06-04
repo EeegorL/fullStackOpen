bc3d1f2f941bf54235a5dc81c99df9420d21fcad
EeegorL <mikkolegezin@gmail.com>
2025-06-04 10:59:54 +0300

    nyt ainaki

--- osa3/puhluettelo_backend/models/Person.js
+++ osa3/puhluettelo_backend/models/Person.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const password = process.env.MONGOOSE_PASSWORD;
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.fh6cxq9.mongodb.net/puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0`;
-
+             
 mongoose.set("strictQuery", false);
 mongoose.connect(url)
 .catch(err => console.log("Kirjautuminen epäonnistui. Salasana muuttunut?", err));
 
 const validation = (val) => {
   try {
+    val = val.trim();
     const parts = val.split("-");
     if (parts.length !== 2) return false;
     if (parts[0].length < 2 || parts[0].length > 3) return false;
