3938f008799b83bf1041d42c2292dad7cfe8b551
EeegorL <mikkolegezin@gmail.com>
2025-11-05 19:55:57 +0200

    4 osa alusta tauon jälkee

--- osa4/blogit/utils/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const PORT = process.env.PORT;
-const MONGO_URI = process.env.NODE_ENV === "production"
-? process.env.MONGO_URI
-: process.env.MONGO_TEST_URI;
-
-module.exports = {PORT, MONGO_URI};
--- osa4/blogit/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const log = (...data) => {
-    console.log(...data);
-};
-
-const error = (...data) => {
-    console.error(...data);
-};
-
-module.exports = {log, error};
